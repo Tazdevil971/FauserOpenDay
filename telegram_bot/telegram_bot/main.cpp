009e3582fbffa7c0d47812dc1c7ac1ecfd01ceeb
unknown <tazdevil971@gmail.com>
2019-11-16 19:32:14 +0100

    Aggiunto return nel caso non ci siano sufficienti argomenti

--- telegram_bot/telegram_bot/main.cpp
+++ telegram_bot/telegram_bot/main.cpp
@@ -188,8 +188,10 @@ private:
 
 int main(int argc, char** argv) {
 
-	if (argc != 2)
+	if (argc != 2) {
 		std::cout << "Wrong number of arguments" << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	try { 
 		App app(argv[1]);
